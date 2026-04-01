7edb9dbc725ac4948cac089c59b1e583eb00491c
TamerHosny <thosnybio@gmail.com>
2026-04-01 08:20:04 +0300

    Add new files

---
+++ routes/base.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI,APIRouter
+import os
+
+
+base_router = APIRouter(
+    prefix="/api/v1",
+    tags=["api_v1"]
+)
+
+@base_router.get("/")
+async def welcome():
+    app_name = os.getenv('APP_NAME')
+    app_ver = os.getenv('APP_VERSION')
+    return {
+        "app_name": app_name,
+        "app_ver": app_ver,
+    }
+
+@base_router.get("/test")
+def test():
+    return{
+        "message": "TEST2"
+    }
